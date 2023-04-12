88b788b24c207303ace7b40052e002bffe70f804
Ruben David <rupedavecoo25@gmail.com>
2023-04-12 18:29:57 -0400

    Aplicando Estilo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Testimonio from './componentes/Testimonio'
 function App() {
   return (
     <div className="App">
-      <Testimonio />
+      <div className='contnedor principal'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimonio />
+      </div>
     </div>
   );
 }
